7b3a6b6a41b6c9c5ae11a3717824e099269e7944
Biplov Bhandari <bionicbiplov45@gmail.com>
2024-03-22 18:15:42 -0400

    docs changes cookiecutter templating - more to come

---
+++ tests/test_aces.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `aces` package."""
+
+
+import unittest
+
+from aces import aces
+
+
+class TestAces(unittest.TestCase):
+    """Tests for `aces` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
